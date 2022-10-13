24aea162a4bed4d0a54ee5ff73ab734413984a85
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-10-13 13:56:19 +0530

    Address Book

---
+++ AddressBookManager.java
@@ -0,0 +1,11 @@
+package Day27;
+
+public class AddressBookManager {
+
+	public static void main(String[] args) {
+		AddressBook entry = new AddressBook();
+	
+		System.out.println("Welcome to Address Book Program in Java!");
+		entry.manageAddressBookList();
+	}
+}
